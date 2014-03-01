f09cbe1d48b791e86ea905fa72706ab1670ea477
Harry Whelchel <apprentice@DBC-Infinity-4.local>
2014-03-01 15:17:00 -0800

    Add event listener on click me button. Add event handler which submits an ajax post request to the /color route. Route returns hex value of a color.

--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,3 +1,24 @@
-$(document).ready(function(){
+var $Wrapper = {
 
+  getColor: function(){
+    $('a#get_color').click(function(e){
+      e.preventDefault();
+
+      $.ajax({
+        type: 'POST',
+        url:  '/color',
+        success: function(resp){
+          console.log(resp)
+        },
+        error: function(){
+          console.log('error!')
+        }
+      });
+    });
+  }
+}
+
+$(function(){
+
+  $Wrapper.getColor();
 });

49569b1c6c1831eecd8cde29b9ae592a9fdc50bc
Harry Whelchel <apprentice@DBC-Infinity-4.local>
2014-03-01 16:22:00 -0800

    Refactor 2.0 to MVC.
    -I don't really have a model? Is that ok?
    -Controller sends AJAX request? I thought that made sense but not sure
    where it should be.
    -Is defining a bunch of variables at the top of a function good or bad
    practice? Should they be specified outside and passed in as a config
    object?
    -View functions cellSelector and cssStyle are a little wonky IMO.
    Any thoughts on cleaning them up would be appreciated.

--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,45 +1,127 @@
-var $Wrapper = {
+// App Object and Selector
+var SaysApp = {}
+targets = {
+  colorEventSelector: 'a#get_color'
+}
 
-  getColor: function(){
-    var wrapper = this
-    var sel = 'a#get_color'
+// Binder
+SaysApp.Binder = function(targets, controller) {
+  this.targets = targets
+  this.controller = controller
+}
+
+SaysApp.Binder.prototype = {
+  bind: function(){
+    var controller = this.controller
+    var sel = targets.colorEventSelector
+
     $(sel).click(function(e){
       e.preventDefault();
-      wrapper.requestColor();
-    });
-  },
+      controller.requestColor();
+    })
+  }
+}
 
+// Controller
+SaysApp.Controller = function(config) {
+  this.view = config.view
+}
+
+SaysApp.Controller.prototype = {
   requestColor: function(){
     var request = 'POST'
     var route = '/color'
     var error = 'error!'
-    var wrapper = this
+    var controller = this
+    var view = this.view
     $.ajax({
       type: request,
       url:  route,
       success: function(resp){
-        wrapper.updateCellColor(JSON.parse(resp));
+        controller.resp = JSON.parse(resp)
+        view.update(controller);
       },
-      error: function(){
-        console.log(error);
+      error: function(resp){
+        controller.resp = JSON.parse(resp)
+        view.update(controller);
       }
     });
-  },
-
-  updateCellColor: function(resp) {
-    console.log(this.cssStyle(resp.color));
-    $(this.cellSelector(resp.cell)).css(this.cssStyle(resp.color));
-  },
-
-  cellSelector: function(cell){
-    return 'ul li:nth-child(' + cell + ')'
-  },
-
-  cssStyle: function(color) {
-    return {'background-color': color}
   }
 }
 
+
+// View
+SaysApp.View = function(){
+}
+
+SaysApp.View.prototype = {
+  update: function(dataSource) {
+    if (dataSource.resp.cell) {
+      this.updateCellColor(dataSource.resp);
+    } else {
+      this.showError(dataSource.resp);
+    }
+  },
+  updateCellColor: function(resp) {
+    $(this.cellSelector(resp.cell)).css(this.cssStyle(resp.color));
+  },
+  cellSelector: function(cell){
+    return 'ul li:nth-child(' + cell + ')'
+  },
+  cssStyle: function(color) {
+    return {'background-color': color}
+  },
+  showError: function(resp){
+    console.log('Sorry did not work!');
+    console.log(resp);
+  }
+}
+
+// Refactor 1.0
+// var $Wrapper = {
+
+//   getColor: function(){
+//     var wrapper = this
+//     var sel = 'a#get_color'
+//     $(sel).click(function(e){
+//       e.preventDefault();
+//       wrapper.requestColor();
+//     });
+//   },
+
+//   requestColor: function(){
+//     var request = 'POST'
+//     var route = '/color'
+//     var error = 'error!'
+//     var wrapper = this
+//     $.ajax({
+//       type: request,
+//       url:  route,
+//       success: function(resp){
+//         wrapper.updateCellColor(JSON.parse(resp));
+//       },
+//       error: function(){
+//         console.log(error);
+//       }
+//     });
+//   },
+
+//   updateCellColor: function(resp) {
+//     $(this.cellSelector(resp.cell)).css(this.cssStyle(resp.color));
+//   },
+
+//   cellSelector: function(cell){
+//     return 'ul li:nth-child(' + cell + ')'
+//   },
+
+//   cssStyle: function(color) {
+//     return {'background-color': color}
+//   }
+// }
+
 $(function(){
-  $Wrapper.getColor();
+  // $Wrapper.getColor();
+  SaysApp.controller = new SaysApp.Controller({view: new SaysApp.View()})
+  SaysApp.binder = new SaysApp.Binder(targets, SaysApp.controller)
+  SaysApp.binder.bind();
 });
